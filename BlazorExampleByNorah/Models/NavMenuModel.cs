514dd48d44bd9e6478a52ed47b592d0b9b45e246
Jeongweon Kim <norah@wisdomain.com>
2020-12-24 10:52:37 +0900

    Revert "Revert "NavMenu 수정숭""
    This reverts commit cf15a32b9885ca123f47cb59c74895f65e11e9ee.

---
+++ BlazorExampleByNorah/Models/NavMenuModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExampleByNorah.Models
+{
+    public class NavMenuModel
+    {
+        public string text { get; set; }
+        public string href { get; set; }
+
+        public bool hasSubMenu { get; set; }
+        public List<NavSubMenu> subMenu { get; set; }
+}
+    public class NavSubMenu
+    {
+        public string text { get; set; }
+        public string href { get; set; }
+    }
+}
