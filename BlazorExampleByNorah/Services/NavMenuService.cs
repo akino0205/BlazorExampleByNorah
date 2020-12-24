49990bbf0d882132ed978f88e5b9750e2c7583b4
Jeongweon Kim <norah@wisdomain.com>
2020-12-24 17:46:45 +0900

    NavMenu Component 장착 완료!

--- BlazorExampleByNorah/Services/NavMenuService.cs
+++ BlazorExampleByNorah/Services/NavMenuService.cs
@@ -1,7 +1,9 @@
 ﻿using BlazorExampleByNorah.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BlazorExampleByNorah.Services
@@ -12,9 +14,45 @@ namespace BlazorExampleByNorah.Services
         {
             List<NavMenuModel> menus = new List<NavMenuModel>();
 
-            NavMenuModel model = new NavMenuModel();
-            menus.Add(model);
+            string pageFolderPath = $"{ Directory.GetCurrentDirectory()}\\Pages";
+            List<string> pageFolderList = new List<string>(Directory.EnumerateDirectories(pageFolderPath));
+            foreach(string currentFullPath in pageFolderList)
+            {
+                NavMenuModel model = new NavMenuModel();
+                string currentPageFolder = currentFullPath.Replace(pageFolderPath, "");
+                model.text = currentPageFolder;
+                bool hasMultiFiles = Directory.GetFiles(currentFullPath).Length > 1;
+                bool hasMultiFolders = Directory.GetDirectories(currentFullPath).Length > 1;
 
+                if(hasMultiFiles || hasMultiFolders)
+                {
+                    model.hasSubMenu = true;
+                    List<string> fullPathList;
+                    if (hasMultiFiles)
+                        fullPathList = new List<string>(Directory.EnumerateFiles(currentFullPath));
+                    else
+                        fullPathList = new List<string>(Directory.EnumerateDirectories(currentFullPath));
+
+                    var subMenuList = new List<NavSubMenu>();
+                    foreach(var fullpath in fullPathList)
+                    {
+                        NavSubMenu subMenuModel = new NavSubMenu();
+                        string subPageName = fullpath.Replace($"{pageFolderPath}{currentPageFolder}", "").Split('\\')[1];
+                        if(hasMultiFiles) subPageName = subPageName.Split('.')[0];
+                        subMenuModel.text = subPageName;
+                        subMenuModel.href = $"{currentPageFolder}/{subPageName}";
+                        subMenuList.Add(subMenuModel);
+                    }
+                    model.subMenu = subMenuList;
+                }
+                else
+                {
+                    model.hasSubMenu = false;
+                    model.href = currentPageFolder;
+                }
+
+                menus.Add(model);
+            }
             return menus;
         }
     }
