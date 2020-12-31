d96d09014a933cad0a2f40738ae6ed6653073676
Jeongweon Kim <norah@wisdomain.com>
2020-12-31 11:51:35 +0900

    InputEx - CheckAll 추가

---
+++ BlazorExampleByNorah/Models/InputEx/CheckboxModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExampleByNorah.Models
+{
+    public class CheckboxModel
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+        public bool isChecked { get; set; }
+        public bool isDisable { get; set; }
+        public string checkboxStyle { get; set; }
+    }
+}
