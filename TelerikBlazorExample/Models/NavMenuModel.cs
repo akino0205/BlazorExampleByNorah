43332395f364c37094f613981915e6f8f411f411
Jeongweon Kim <norah@wisdomain.com>
2021-01-15 18:32:50 +0900

    Nav Component 및 디자인 적용

---
+++ TelerikBlazorExample/Models/NavMenuModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TelerikBlazorExample.Models
+{
+    public class NavMenuModel
+    {
+        public string text { get; set; }
+        public string href { get; set; }
+
+        public bool hasSubMenu { get; set; }
+        public List<NavSubMenu> subMenu { get; set; }
+}
+    public class NavSubMenu
+    {
+        public string text { get; set; }
+        public string href { get; set; }
+    }
+}
--- TelerikBlazorExample/Startup.cs
+++ TelerikBlazorExample/Startup.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TelerikBlazorExample.Data;
+using TelerikBlazorExample.Services;
 
 namespace TelerikBlazorExample
 {
@@ -29,6 +30,9 @@ namespace TelerikBlazorExample
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            //NavMenu
+            services.AddScoped<NavMenuService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
